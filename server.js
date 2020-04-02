243c3728896fe3219fae992b76566264917b7829
Bailey Garrett <baileyagarrett@gmail.com>
2020-04-01 22:31:16 -0500

    trying this again

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ mongoose.connect(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/public", "index.html"));
-  });
+  app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "./client/public", "index.html"));
+  // });
 }
 
 // Add routes, both API and view
