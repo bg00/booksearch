da05cbd2018d8eec3a39b9aa55a7fd62f5b00011
Bailey Garrett <baileyagarrett@gmail.com>
2020-04-01 22:15:56 -0500

    removed /build ignore from .gitignore

---
+++ client/build/precache-manifest.c53e20e4fc1d7ec352bf617e5384a235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87374b4ae0301e4608734d351e829c53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f620d544ff45c270024",
+    "url": "/static/css/2.9bde7c98.chunk.css"
+  },
+  {
+    "revision": "b75a5ba88274f92f251b",
+    "url": "/static/css/main.560cd6b6.chunk.css"
+  },
+  {
+    "revision": "9f620d544ff45c270024",
+    "url": "/static/js/2.9eea4485.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9eea4485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75a5ba88274f92f251b",
+    "url": "/static/js/main.4dac8b54.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  }
+]);
