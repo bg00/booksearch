6dbab27bad80725dbf20328b508253767ce20da7
Bailey Garrett <baileyagarrett@gmail.com>
2020-04-01 21:29:45 -0500

    updating route

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ mongoose.connect(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "./client/public", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public", "index.html"));
+  });
 }
 
 // Add routes, both API and view
